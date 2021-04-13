6a34ee9c50c15852f25043551f55df4d28542951
Sienkiewicz <peter@sinkevich.com.ua>
2021-04-13 15:28:04 +0200

    add check does detchData has data

--- src/index.ts
+++ src/index.ts
@@ -13,18 +13,18 @@ const NAME_OF_COOKIE = 'vendorsCookie'
 window.addEventListener('DOMContentLoaded', async event => {
   // check did cookies is present
   if (!getCookie(NAME_OF_COOKIE)) {
-    // set DOM element with CSS changes
-    const cont = document.getElementById('container')
-    const body = document.getElementById('blur')
-    cssChanges(cont, body)
-    changeScroll('remove')
-
     // get list of Vendors
     const partnersList = await fetchData()
+    if (partnersList) {
+      // set DOM element with CSS changes
+      const cont = document.getElementById('container')
+      const body = document.getElementById('blur')
+      cssChanges(cont, body)
+      changeScroll('remove')
 
-    // Set form in the DOM with list of vendors
-    cont &&
-      (cont.innerHTML = `
+      // Set form in the DOM with list of vendors
+      cont &&
+        (cont.innerHTML = `
       <form id='form-cookies'>
         <h1>GDPR consent</h1>
         <div>
@@ -37,29 +37,30 @@ window.addEventListener('DOMContentLoaded', async event => {
       </form>
       `)
 
-    // register buton and form
-    const rejectBtn = document.getElementById('btn-reject')
-    const form = document.forms.namedItem('form-cookies')
+      // register buton and form
+      const rejectBtn = document.getElementById('btn-reject')
+      const form = document.forms.namedItem('form-cookies')
 
-    // set submit function with setCookies settings and CSS settings after click
-    form &&
-      (form.onsubmit = e => {
-        e.preventDefault()
-        let vendorsList: string[] = []
-        for (let id of new FormData(form).keys()) {
-          vendorsList.push(id)
-        }
-        setCookie(NAME_OF_COOKIE, vendorsList.join(','), 1)
-        changeScroll('add')
-        cssChanges(cont, body, 'remove')
-      })
+      // set submit function with setCookies settings and CSS settings after click
+      form &&
+        (form.onsubmit = e => {
+          e.preventDefault()
+          let vendorsList: string[] = []
+          for (let id of new FormData(form).keys()) {
+            vendorsList.push(id)
+          }
+          setCookie(NAME_OF_COOKIE, vendorsList.join(','), 1)
+          changeScroll('add')
+          cssChanges(cont, body, 'remove')
+        })
 
-    // Set CSS settings when button will be clicked
-    rejectBtn &&
-      rejectBtn.addEventListener('click', e => {
-        e.preventDefault()
-        cssChanges(cont, body, 'remove')
-        changeScroll('add')
-      })
+      // Set CSS settings when button will be clicked
+      rejectBtn &&
+        rejectBtn.addEventListener('click', e => {
+          e.preventDefault()
+          cssChanges(cont, body, 'remove')
+          changeScroll('add')
+        })
+    }
   }
 })
